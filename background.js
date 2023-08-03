f9dbc641cee5e60edfecb3d08cf343f55b2794f0
wangyouquanMas <wyq1995dr@163.com>
2023-08-03 13:39:28 +0800

    update

---
+++ background.js
@@ -0,0 +1,60 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+     var selectedText = request.text;
+      console.log(selectedText);
+   
+    if (request.type === "selectedText") {
+    
+        console.log('Received selected text:', selectedText);
+
+        const api_key = "secret_EojgyhKkKaSD28Mbwh5bOlC8aCNEtu3AUExsgNDnDYk";
+        const parent_page_id = "abfee6c23d674cc0a24ba1e22fc38703";
+
+        const headers = {
+            "Authorization": "Bearer " + api_key,
+            "Content-Type": "application/json",
+            "Notion-Version": "2022-06-28"
+        };
+
+        const payload = {
+            "parent": {"page_id": parent_page_id},
+            "properties": {
+                "title": {
+                    "title": [
+                        {
+                            "text": {
+                                "content": selectedText
+                            }
+                        }
+                    ]
+                }
+            },
+            "children": [
+                {
+                    "object": "block",
+                    "type": "paragraph",
+                    "paragraph": {
+                        "rich_text": [
+                            {
+                                "type": "text",
+                                "text": {
+                                    "content": "Text selected from the webpage: " + selectedText
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
+        };
+
+        fetch('https://api.notion.com/v1/pages', {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(payload)
+        }).then(response => response.json())
+        .then(data => console.log('Response from Notion:',data))
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    }
+    return true; 
+});
---
+++ content.js
@@ -0,0 +1,39 @@
+document.addEventListener('mouseup', function() {
+    // Get the selected text
+    var selectedText = window.getSelection().toString().trim();
+
+    console.log('Selected text:', selectedText);
+    let message = selectedText;
+
+    chrome.runtime.sendMessage("ojiljljdfccjamehladmhhkooahmlkol",{type:"selectedText",text:message},response => {
+      //    // Close the options page
+      // close();
+      // if (!chrome.runtime.lastError || !retry) {
+      //   clearTimeout(timer);
+      //   callback(response);
+      // }
+    });
+    // send(message, handleResponse, handleError);
+  });
+  
+  function handleResponse(response) {
+    console.log(`Message from the background script: ${response}`);
+  }
+  
+  function handleError(error) {
+    console.log(`Error: ${error}`);
+  }
+  
+  function send(msg, callback, retry = 20) {
+    const timer = setTimeout(() => send(msg, callback, retry - 1), 100);
+    
+   chrome.runtime.sendMessage("ojiljljdfccjamehladmhhkooahmlkol",{type:"selectedText",text:msg},response => {
+      //    // Close the options page
+      // close();
+      if (!chrome.runtime.lastError || !retry) {
+        clearTimeout(timer);
+        callback(response);
+      }
+    });
+  }
+  
